5fe966f0896b97a9f585105ae1d14a0cd4e571ee
Anil Kumar <anil3559@gmail.com>
2023-08-10 04:22:23 +0530

    final push

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,45 +1,79 @@
+import React, { useState } from 'react';
 import Navbar from "../../components/Navbar";
 import Link from "next/link";
 import Image from 'next/image';
-import React, { useState } from 'react';
+
 import { RiDeleteBinLine} from 'react-icons/ri';
+import {BsSearch} from "react-icons/bs";
 
 
 export const getStaticProps = async () => {
+    //comment fetch
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const data = await res.json();
+    //user fetch
+      const res2 = await fetch("https://randomuser.me/api/?results=50");
+      const data2 = await res2.json();
+     const data3 = data2.results;
 
 
-  // const data4 = await res.json();
-
-
-  const res = await fetch("https://jsonplaceholder.typicode.com/comments");
-  const data = await res.json();
-
-  const res2 = await fetch("https://randomuser.me/api/?results=50");
-  const data2 = await res2.json();
- const data3 = data2.results;
-
- //const [query, setQuery] = useState("");
 
   return {
     props: {
-      data,data2,data3,
+      data,data3,
     },
   };
 };
 
-const blog = ({ data,data2,data3}) => {
+const blog = ({ data,data3}) => {
+    const commentList  = [];
+     data.slice(0, 50).map((curElem2) => {
 
+        // let comment_array:[] = curElem2.body;
+         const commentList2 = {
+  id: curElem2.id,
+  body: curElem2.body
+};
+        commentList.push(commentList2);
+//console.log(commentList);
+        //return ;
+    });
+console.log(commentList);
 
+    const [comments, setComments] = useState(commentList);
+    const [searchVal, setSearchVal] = useState("");
+
+    function handleSearchClick () {
+        if (searchVal === "") {
+            setComments(commentList);
+            //console.log(searchVal);
+            //console.log('all');
+            return;
+        }
+        const filterBySearch = commentList.filter((item) => {
+            if (item.body.toLowerCase()
+                .includes(searchVal.toLowerCase())) {
+                // console.log(item);
+                // console.log('search');
+                return item;
+             }
+        })
+        setComments(filterBySearch);
+    }
+
+//console.log(comments);
   return (
     <>
       <Navbar />
-<div className="ssr-styles3">
- <input type="text" className ="search_boxx" placeholder="search"  />
+<div className="ssr-styles3"><BsSearch className="search_icon" onClick={handleSearchClick} />
+ <input type="text" className ="search_boxx" placeholder="search" onChange={e => setSearchVal(e.target.value)} />
+
 </div>
-      {data.slice(0, 5).map((curElem,i) =>
+
+      { comments.slice(0, 50).map((curElem,i) =>
 
       {
-console.log(data3[i]);
+//console.log(data3[i]);
 //const[count,setCount]=useState(0);
  const isEvenOdd = (count)=>{
      return (count%2===0)
